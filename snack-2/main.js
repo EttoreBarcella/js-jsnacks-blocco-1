f866c8dacf645fd5a416cb7c65a8b0fe44d07607
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-04 11:53:57 +0200

    added comment

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,11 +1,16 @@
-
+// chiedo la prima parola
 let firstWord = prompt("inserisci una parola")
+// mi segno quanto è lunga
 let lenghtFirstWord = firstWord.length
 console.log(lenghtFirstWord)
+// chiedo la seconda parola
 let secondWord = prompt("inserisci un'altra parola")
+// mi segno quanto è lunga
 let lenghtSecondWord = secondWord.length
 console.log(lenghtSecondWord)
-
+// se prima > seconda stampo la prima
+// invece se prima < seconda stampo la seconda
+// altrimenti se sono uguali scrivo che le parole sono lunghe uguali
 if (lenghtFirstWord > lenghtSecondWord) {
     console.log(firstWord)
 } else if (lenghtFirstWord < lenghtSecondWord) {
