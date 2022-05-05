31ced59c4746c0043031dfc9e0d7de2442307c70
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-05 10:06:00 +0200

    added more logic to snack 9

--- snack-9/main.js
+++ snack-9/main.js
@@ -4,15 +4,20 @@
 // creo l'array della prima lista
 const firstList = [1 , 3 , 5 , 6 , 7]
 // creo l'array della seconda lista
-const secondList = [ 2 , 10]
-// creo un generatore random di numeri
+const secondList = [ 2 , 10 , 4 , 5 , 6 , 7 , 8 , 8]
+// creo un generatore random di numeri da aggiungere alla lista meno grande
 let randomNum = Math.floor(Math.random() * 10);
 // creo un ciclo
 for (let i = 0 ; i <= firstList.length ; i++){
     // se la seconda lista è più corta allora aggiungo un numero casuale nella seconda lista
     if (secondList.length < firstList.length){
         secondList.push(randomNum)
+    // se la prima lista è più corta allora aggiungo un numero casuale nella prima lista
+    } else if (secondList.length > firstList.length){
+        firstList.push(randomNum)
     }
 }
+// Stampo la prima lista in console
+console.log(firstList)
 // Stampo la seconda lista in console
 console.log(secondList)
