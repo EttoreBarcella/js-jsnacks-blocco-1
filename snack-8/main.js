611bb4e5ec4b3f09a5da5dfff65f18d285c30f46
EttoreBarcella <ettore.barcella@gmail.com>
2022-05-05 09:56:35 +0200

    added snack 9 + logic

--- snack-8/main.js
+++ snack-8/main.js
@@ -2,9 +2,10 @@
 // e fai la somma di tutti gli elementi  che sono in posizione dispari
 
 // Creo array di numeri interi
-const numberList = [1 , 2 , 3 , 4 , 5]
+// 1 = 0, 2 = 1 , 3 = 2 , 4 = 3 , 5 = 4 , 6 = 5
+const numberList = [1 , 2 , 3 , 4 , 5 , 6]
 
-// faccio la somma degli indici in posizione dispari => 2 e 4
+// faccio la somma degli indici in posizione dispari => 2 , 4 e 6
 let sum = 0
 for (let i = 0 ; i < numberList.length ; i++){
     // SE i === 1 allora dispari e sommo a variabile Sum che è contenitore dei miei indici dispari
---
+++ snack-9/main.js
@@ -0,0 +1,18 @@
+// Crea due array che hanno un numero di elementi diversi. 
+// Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+// creo l'array della prima lista
+const firstList = [1 , 3 , 5 , 6 , 7]
+// creo l'array della seconda lista
+const secondList = [ 2 , 10]
+// creo un generatore random di numeri
+let randomNum = Math.floor(Math.random() * 10);
+// creo un ciclo
+for (let i = 0 ; i <= firstList.length ; i++){
+    // se la seconda lista è più corta allora aggiungo un numero casuale nella seconda lista
+    if (secondList.length < firstList.length){
+        secondList.push(randomNum)
+    }
+}
+// Stampo la seconda lista in console
+console.log(secondList)
